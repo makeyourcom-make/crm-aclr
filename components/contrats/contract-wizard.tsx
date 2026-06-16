0f4698d78893401efdea9f2019062afa8a5e891c
Arthur Chazelle <contact@makeyourcom.ch>
2026-06-16 17:59:31 +0200

    fix(contrat): retirer le mot 'commission' du libelle Details internes (invisible au client)

--- components/contrats/contract-wizard.tsx
+++ components/contrats/contract-wizard.tsx
@@ -761,7 +761,7 @@ export function ContractWizard({
               name={showInternal ? "ChevronDown" : "ChevronRight"}
               className="h-3.5 w-3.5"
             />
-            Détails internes (commission, automatisations)
+            Détails internes
           </button>
 
           {showInternal && (
